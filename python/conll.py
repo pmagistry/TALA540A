db32f0c5b6e0d291e59cfcabc93476d52fe45d6c
Sazuna <liza.fretel@gmail.com>
2023-10-09 19:51:53 +0200

    Remove old code (pandas dataframe)

--- python/conll.py
+++ python/conll.py
@@ -1,51 +1,9 @@
 #!/bin/python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import pandas as pd
 import sys
 
-from io import StringIO
 from corpus import Corpus, Document, Sentence, Token
-from typing import List
-
-def get_sentences(file: str) -> list:
-	file = open(file, "r")
-	data = file.read()
-	file.close()
-
-	sentences = "\n".join([s for s in data.split("\n") if not s.startswith("#")])
-	sentences = sentences.split("\n\n")
-	
-	return sentences
-
-def get_header(file: str) -> list:
-	file = open(file, "r")
-	line = file.readline()
-	while not line.startswith("# global.columns"):
-		line = file.readline()
-	line = line.split('=')[1].split(' ')[1:]
-	return line
-
-def sentence_to_np(sentence: str, head: list):
-	#csvStringIO = StringIO(sentence)
-	#df = pd.read_csv(csvStringIO, sep='\t')	
-	#df = pd.read_csv(sentence)	
-	table = []
-	for line in sentence.split("\n"):
-		line_table = []
-		for field in line.split("\t"):
-			line_table.append(field)
-		table.append(line_table)
-
-	try:
-		df = pd.DataFrame(table, columns=head)
-		return df
-	except:
-		print("ERROR ON THIS TABLE:")
-		print(table)
-		print("FORMAT MISMATCH FOR HEADER:")
-		print(head)
 
 def conll_to_corpus(file: str) -> Corpus:
 	file = open(file, 'r')
@@ -60,10 +18,6 @@ def conll_to_corpus(file: str) -> Corpus:
 
 	sent_tokens = tokens_string.split("\n\n")
 	sent_string = sentences.split("\n")
-	"""
-	print(sent_tokens[0])
-	print(sent_string[0])
-	"""
 
 	s = [] # list of Sentence objects
 
