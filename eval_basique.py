de1de6a83725e55989047acf034b8c35c7cbb4df
Pierre M <pierre.magistry@inalco.fr>
2023-10-18 19:14:14 +0200

    passage de mypy et black

--- eval_basique.py
+++ eval_basique.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Dict, Set, Optional
+from typing import List, Union, Dict, Set, Optional, Tuple
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -6,54 +6,63 @@ from spacy import Language as SpacyPipeline
 from spacy.tokens import Token as SpacyToken, Doc as SpacyDoc
 import spacy
 
-@dataclass 
+
+@dataclass
 class Token:
     form: str
     tag: str
     is_oov: bool
 
+
 @dataclass
 class Sentence:
     tokens: List[Token]
+
 
 @dataclass
 class Corpus:
     sentences: List[Sentence]
 
 
-def read_conll(path: Path, vocabulaire: Optional[Set[str]]=None ) -> Corpus:
-    sentences = []    
-    tokens = []
+def read_conll(path: Path, vocabulaire: Optional[Set[str]] = None) -> Corpus:
+    sentences: List[Sentence] = []
+    tokens: List[Token] = []
     with open(path) as f:
         for line in f:
             line = line.strip()
             if not line.startswith("#"):
-                
                 if line == "":
                     sentences.append(Sentence(tokens))
                     tokens = []
                 else:
                     fields = line.split("\t")
                     form, tag = fields[1], fields[3]
-                    if not "-" in fields[0]: # éviter les contractions type "du"
+                    if not "-" in fields[0]:  # éviter les contractions type "du"
                         if vocabulaire is None:
-                            is_oov=True
+                            is_oov = True
                         else:
                             is_oov = not form in vocabulaire
-                        tokens.append(Token(form, tag, is_oov ))
+                        tokens.append(Token(form, tag, is_oov))
     return Corpus(sentences)
+
+
+def build_vocabulaire(corpus: Corpus) -> Set[str]:
+    return {tok.form for sent in corpus.sentences for tok in sent.tokens}
+
 
 def sentence_to_doc(sentence: Sentence, vocab) -> SpacyDoc:
     words = [tok.form for tok in sentence.tokens]
     return SpacyDoc(vocab, words=words)
 
+
 def doc_to_sentence(doc: SpacyDoc, origin: Sentence) -> Sentence:
     tokens = []
     for tok, origin_token in zip(doc, origin.tokens):
-        tokens.append(Token(tok.text, tok.pos_, is_oov=origin_token.is_oov)) 
+        tokens.append(Token(tok.text, tok.pos_, is_oov=origin_token.is_oov))
     return Sentence(tokens)
 
-def tag_corpus_spacy(corpus: Corpus, model_spacy: SpacyPipeline ) -> Corpus:
+
+def tag_corpus_spacy(corpus: Corpus, model_spacy: SpacyPipeline) -> Corpus:
     sentences = []
     for sentence in corpus.sentences:
         doc = sentence_to_doc(sentence, model_spacy.vocab)
@@ -62,14 +71,16 @@ def tag_corpus_spacy(corpus: Corpus, model_spacy: SpacyPipeline ) -> Corpus:
     return Corpus(sentences)
 
 
-def compute_accuracy(corpus_gold: Corpus, corpus_test:Corpus) -> float:
+def compute_accuracy(corpus_gold: Corpus, corpus_test: Corpus) -> Tuple[float, float]:
     nb_ok = 0
     nb_total = 0
     oov_ok = 0
     oov_total = 0
-    for sentence_gold, sentence_test in zip(corpus_gold.sentences, corpus_test.sentences):
+    for sentence_gold, sentence_test in zip(
+        corpus_gold.sentences, corpus_test.sentences
+    ):
         for token_gold, token_test in zip(sentence_gold.tokens, sentence_test.tokens):
-            assert(token_gold.form == token_test.form)
+            assert token_gold.form == token_test.form
             if token_gold.tag == token_test.tag:
                 nb_ok += 1
             nb_total += 1
@@ -79,8 +90,6 @@ def compute_accuracy(corpus_gold: Corpus, corpus_test:Corpus) -> float:
                     oov_ok += 1
 
     return nb_ok / nb_total, oov_ok / oov_total
-
-
 
 
 def main():
@@ -93,36 +102,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
