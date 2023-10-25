54b9d3efa9caea84932966e441152c7a4f34dda4
Orane Dufour <orane.dufour@gmail.com>
2023-10-25 18:06:54 +0200

    correction oov + ajout matrice de confusion png

--- eval_basique.py
+++ eval_basique.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Dict, Set
+from typing import List, Union, Dict, Set, Optional, Tuple
 from pathlib import Path
 from dataclasses import dataclass
 from collections import defaultdict
@@ -8,7 +8,8 @@ from spacy.tokens import Token as SpacyToken, Doc as SpacyDoc
 import spacy
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import argparse
+#from pyJoules.energy_meter import measure_energy
 
 @dataclass 
 class Token():
@@ -18,6 +19,7 @@ class Token():
 
 @dataclass
 class Sentence():
+    #sent_id : str
     tokens: List[Token]
 
 @dataclass
@@ -25,13 +27,15 @@ class Corpus():
     sentences: List[Sentence]
 
 
-def read_conll(path: Path, vocabulaire : [set[str]]=None) -> Corpus:
+def read_conll(path: Path, vocabulaire : Optional[Set[str]]=None) -> Corpus:
     sentences = []    
     tokens = []
     vocab = set()
     with open(path) as f:
         for line in f:
             line = line.strip()
+            if "sent_id" in line :
+                sent_id = line.split("=")[1].strip()
             if not line.startswith("#"):
                 
                 if line == "":
@@ -49,7 +53,10 @@ def read_conll(path: Path, vocabulaire : [set[str]]=None) -> Corpus:
                         tokens.append(Token(form,tag,is_oov))
                         vocab.add(form)
 
-    return Corpus(sentences), vocab
+    return Corpus(sentences)
+
+def build_vocabulaire(corpus: Corpus) -> Set[str]:
+    return {tok.form for sent in corpus.sentences for tok in sent.tokens}
 
 def sentence_to_doc(sentence: Sentence, vocab) -> SpacyDoc:
     words = [tok.form for tok in sentence.tokens]
@@ -61,6 +68,7 @@ def doc_to_sentence(doc: SpacyDoc) -> Sentence:
         tokens.append(Token(tok.text, tok.pos_, is_oov=tok.is_oov)) # bof
     return Sentence(tokens) 
 
+
 def tag_corpus_spacy(corpus: Corpus, model_spacy: SpacyPipeline ) -> Corpus:
     sentences = []
     for sentence in corpus.sentences:
@@ -70,24 +78,30 @@ def tag_corpus_spacy(corpus: Corpus, model_spacy: SpacyPipeline ) -> Corpus:
     return Corpus(sentences)
 
 
-def compute_accuracy(corpus_gold: Corpus, corpus_test:Corpus, vocabulaire) -> float:
+def compute_accuracy(corpus_gold: Corpus, corpus_test:Corpus, subcorpus: Optional[str] = None) -> float:
     nb_ok = 0
     nb_total = 0
+    oov_ok = 0
     oov_total = 0
     vp = defaultdict(int)
     fp = defaultdict(lambda: defaultdict(int))
     for sentence_gold, sentence_test in zip(corpus_gold.sentences, corpus_test.sentences):
-        for token_gold, token_test in zip(sentence_gold.tokens, sentence_test.tokens):
-            assert(token_gold.form == token_test.form)
-            if token_gold.tag == token_test.tag:
-                vp[token_gold.tag] += 1
-                nb_ok += 1
-            nb_total += 1
-            if token_test.form not in vocabulaire : # est ce que ce token est un oov étant donné le vocabulaire du train
-                oov_total += 1
-                fp[token_gold.tag][token_test.tag] += 1
+        #if subcorpus is None or subcorpus in sentence_gold.sent_id:
+            for token_gold, token_test in zip(sentence_gold.tokens, sentence_test.tokens):
+                assert(token_gold.form == token_test.form)
+                if token_gold.tag == token_test.tag:
+                    vp[token_gold.tag] += 1
+                    nb_ok += 1    
+                    nb_total += 1
+                else : 
+                    nb_total += 1
+                    fp[token_gold.tag][token_test.tag] += 1
+                if token_gold.is_oov : 
+                    oov_total += 1
+                    if token_gold.tag == token_test.tag:
+                        oov_ok += 1
     accuracy = nb_ok / nb_total
-    oov = oov_total / len(vocabulaire)
+    oov = oov_ok / oov_total if oov_total != 0 else 0
 
     return accuracy, oov, vp, fp
 
@@ -95,30 +109,36 @@ def compute_accuracy(corpus_gold: Corpus, corpus_test:Corpus, vocabulaire) -> fl
 def generate_confusion_matrix(VP, FP):
     all_tags = list(set(VP.keys()).union(FP.keys()))
     confusion_matrix = pd.DataFrame(0, index=all_tags, columns=all_tags, dtype=int)
-
-
     for tag, count in VP.items():
         confusion_matrix.at[tag, tag] = count
-
     for real_tag, pred_tags in FP.items():
         for pred_tag, count in pred_tags.items():
             confusion_matrix.at[real_tag, pred_tag] = count
 
     return confusion_matrix
 
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("gold", help = "gold", nargs = "?")
+    parser.add_argument("test", help = "test", nargs = "?")
+    
+    return parser.parse_args()
 
 
 
+
+    
 def main():
-    model_spacy = spacy.load("fr_core_news_sm")
-    corpus_gold, vocab = read_conll("fr_sequoia-ud-test.conllu")
+    corpus_train = read_conll("fr_sequoia-ud-train.conllu")
+    vocab_train = build_vocabulaire(corpus_train)
 
+    model_spacy = spacy.load("fr_core_news_sm")
+    corpus_gold = read_conll("fr_sequoia-ud-test.conllu", vocabulaire=vocab_train)
     corpus_test = tag_corpus_spacy(corpus_gold, model_spacy)
-    accuracy, oov, vp, fp = compute_accuracy(corpus_gold, corpus_test, vocab)
+   
+    accuracy, oov, vp, fp = compute_accuracy(corpus_gold, corpus_test, vocab_train)
     print("accuracy : " , accuracy)
     print("OOV : ", oov )
-    
-    
     
     confusion_data = generate_confusion_matrix(vp, fp)
     
